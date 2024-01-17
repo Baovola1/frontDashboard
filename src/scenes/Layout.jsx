bcb0c24852ab51d916af8188b7262a95d763016e
Baovola <baovolarav@gmail.com>
2024-01-17 16:21:26 +0100

    insert skeleton sidebar

--- src/scenes/Layout.jsx
+++ src/scenes/Layout.jsx
@@ -2,14 +2,18 @@ import React, {useState} from 'react';
 import {Box, useMediaQuery} from "@mui/material";
 import {Outlet} from "react-router-dom";
 import Header from '../components/Header';
+import Sidebar from '../components/Sidebar';
 
 
 export default function Layout() {
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
+  const [sidebarOpen, setSidebarOpen] = useState(true);
   return (
     <>
-    <Box width="100%" height="100%" >
+    <Box display={isNonMobile? "flex": "block"} width="100%" height="100%" >
+      <Sidebar isNonMobile = {isNonMobile} drawerWidth = "250px" sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen}/>
       <Box>
-        <Header/>
+        <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen}/>
         <Outlet/>
       </Box>
     </Box>
