c3e72a913c9a61eb8235ef5fea0f71c8c49fc013
Baovola <baovolarav@gmail.com>
2024-01-17 18:29:43 +0100

    add redux api

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,18 @@ import App from "./App.jsx";
 import "./index.css";
 import { configureStore } from "@reduxjs/toolkit";
 import globalReducer from "./redux";
-
 import { Provider } from "react-redux";
+import {setupListeners} from "@reduxjs/toolkit/query";
+import {api} from "./redux/api.js"
 
 const store = configureStore({
   reducer: {
     global: globalReducer,
+    [api.reducerPath]: api.reducer,
   },
+  middleware:(getDefault) => getDefault().concat(api.middleware)
 });
+setupListeners(store.dispatch);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- src/scenes/Layout.jsx
+++ src/scenes/Layout.jsx
@@ -3,11 +3,20 @@ import {Box, useMediaQuery} from "@mui/material";
 import {Outlet} from "react-router-dom";
 import Header from '../components/Header';
 import Sidebar from '../components/Sidebar';
+import {useGetUserQuery} from "../redux/api";
+import { useSelector } from 'react-redux';
 
 
 export default function Layout() {
   const isNonMobile = useMediaQuery("(min-width: 600px)");
   const [sidebarOpen, setSidebarOpen] = useState(true);
+  const userId = useSelector((state)=>state.global.userId);
+  const {data} = useGetUserQuery(userId);
+  console.log(data);
+
+
+
+
   return (
     <>
     <Box display={isNonMobile? "flex": "block"} width="100%" height="100%" >
