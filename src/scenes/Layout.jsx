5cc78bd0130a85cb9d18f485a1b887d0df800320
Baovola <baovolarav@gmail.com>
2024-01-17 12:25:12 +0100

    implementation dark and light mode Header

--- src/scenes/Layout.jsx
+++ src/scenes/Layout.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react';
+import {Box, useMediaQuery} from "@mui/material";
+import {Outlet} from "react-router-dom";
+import Header from '../components/Header';
+
 
 export default function Layout() {
   return (
-    <div>Layout</div>
+    <>
+    <Box width="100%" height="100%" >
+      <Box>
+        <Header/>
+        <Outlet/>
+      </Box>
+    </Box>
+    </>
+    
   )
 }
